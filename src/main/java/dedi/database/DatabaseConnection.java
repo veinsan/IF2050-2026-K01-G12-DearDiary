c72be5e0eaf53dc512e2ef0c62506b05b03ced36
veinsan <riantamaputra751@gmail.com>
2026-05-09 20:41:38 +0700

    fix: update database name to deardiary

--- src/main/java/dedi/database/DatabaseConnection.java
+++ src/main/java/dedi/database/DatabaseConnection.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
  */
 public class DatabaseConnection {
 
-    private static final String URL      = "jdbc:postgresql://localhost:5432/dedi_db";
+    private static final String URL      = "jdbc:postgresql://localhost:5432/deardiary";
     private static final String USER     = "postgres";
     private static final String PASSWORD = "postgres";
 
