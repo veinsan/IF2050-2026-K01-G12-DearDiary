3c73b50960bddfa3e6bc0baa48e80b3b8a95add1
aldytorafif05 <aldytorafif@gmail.com>
2026-05-12 15:27:11 +0700

    fix: hapus file tambahan

--- src/main/java/dedi/AppLauncher.java
+++
@@ -1,8 +0,0 @@
-package dedi;
-
-public class AppLauncher {
-    public static void main(String[] args) {
-        // Ini akan memanggil Main.java kamu secara diam-diam
-        Main.main(args);
-    }
-}
