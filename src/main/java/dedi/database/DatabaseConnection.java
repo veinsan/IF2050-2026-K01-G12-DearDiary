ba2b09833597789ca2f71c146a355e784a6fb70b
veinsan <riantamaputra751@gmail.com>
2026-05-13 16:47:54 +0700

    refactor: change postgres password to default in DatabaseConnection.java

--- src/main/java/dedi/database/DatabaseConnection.java
+++ src/main/java/dedi/database/DatabaseConnection.java
@@ -23,7 +23,7 @@ public class DatabaseConnection {
 
     private static final String URL      = "jdbc:postgresql://localhost:5432/deardiary";
     private static final String USER     = "postgres";
-    private static final String PASSWORD = "irghisatya8";
+    private static final String PASSWORD = "postgres";
 
     private static Connection instance = null;
 
